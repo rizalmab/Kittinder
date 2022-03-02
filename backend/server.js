8e4f74f8f1970a34d1ff79144257629c300ba3d5
rizalmab <rizalmahadiab@gmail.com>
2022-03-02 15:26:22 +0800

    Connected to MongoDB successfully

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,30 @@
+//* Require method
+// const express = require("express");
+// const mongoose = require("mongoose");
+// require("dotenv").config({ path: "../.env" });
+//* ES6 Import method
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 // App config
 const app = express();
+dotenv.config({ path: "../.env" });
 const PORT = process.env.PORT || 8001;
+console.log("process.env.PORT", process.env.PORT);
+// console.log("process.env.MONGODB_URI", process.env.MONGODB_URI);
 
 // Middleware
 
 // DB Config
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  //   useCreateIndex: true,
+  //   useUnifiedTopology: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB successfully!");
+});
 
 // API endpoints
 app.get("/", (req, res) => {
