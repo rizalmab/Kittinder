2add9f41dfb9d6d34d279b7dea2c35643f18ec68
rizalmab <rizalmahadiab@gmail.com>
2022-03-04 11:14:57 +0800

    Added menu dropdown button

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,12 +1,12 @@
 import React from "react";
 import IconButton from "@material-ui/core/IconButton";
 import ForumIcon from "@material-ui/icons/Forum";
-import Dropdown from "./Dropdown";
+import DropdownMenu from "./DropdownMenu";
 
 const Navbar = () => {
   return (
     <div className="container mx-auto flex items-center justify-between z-100 border-solid border-2 border-black">
-      <Dropdown />
+      <DropdownMenu />
       <img
         className="header_logo object-contain h-14"
         src="logo192.png"
