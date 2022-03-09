96a46e980c672d171a031013ec4010dc103068a7
rizalmab <rizalmahadiab@gmail.com>
2022-03-09 21:41:39 +0800

    Added styling to login and signup pages

--- frontend/src/components/LoginSignup/LoginPage.js
+++ frontend/src/components/LoginSignup/LoginPage.js
@@ -16,10 +16,7 @@ const LoginPage = () => {
     e.preventDefault();
     try {
       const loginUser = { email, password };
-      const loginResponse = await axios.post(
-        "/api/users/login",
-        loginUser
-      );
+      const loginResponse = await axios.post("/api/users/login", loginUser);
       setUserData({
         token: loginResponse.data.token,
         user: loginResponse.data.user,
@@ -36,7 +33,7 @@ const LoginPage = () => {
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-red-700">
               Log in
             </h2>
             {error && (
@@ -110,11 +107,11 @@ const LoginPage = () => {
             <div>
               <button
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                   <LockClosedIcon
-                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                    className="h-5 w-5 text-white"
                     aria-hidden="true"
                   />
                 </span>
--- frontend/src/components/LoginSignup/SignupPage.js
+++ frontend/src/components/LoginSignup/SignupPage.js
@@ -22,13 +22,10 @@ const SignupPage = () => {
       //! send post request to REGISTER
       await axios.post("/api/users/signup", newUser);
       //! send post request to LOGIN
-      const loginResponse = await axios.post(
-        "/api/users/login",
-        {
-          email,
-          password,
-        }
-      );
+      const loginResponse = await axios.post("/api/users/login", {
+        email,
+        password,
+      });
       //! save the token and user in a state
       setUserData({
         token: loginResponse.data.token,
@@ -50,7 +47,7 @@ const SignupPage = () => {
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-red-700">
               Sign up
             </h2>
             {error && (
@@ -130,11 +127,11 @@ const SignupPage = () => {
             <div>
               <button
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                   <LockClosedIcon
-                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                    className="h-5 w-5 text-white "
                     aria-hidden="true"
                   />
                 </span>
